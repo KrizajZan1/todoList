f4516bdebc7f758fe5142bce34408750c8f92c3c
Žan Križaj <77213069+KrizajZan1@users.noreply.github.com>
2024-05-06 11:01:27 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Function for adding tasks via "Enter" key
 function enter(event) {
-  if (event.key === "Enter") {
+  if (event.key == "Enter") {
     addTask();
   }
 }
@@ -9,18 +9,17 @@ function enter(event) {
 function addTask() {
   var taskInput = document.getElementById("taskInput");
   var taskText = taskInput.value;
-  if (taskText !== "") {
+  if (taskText != "") {
     var newTask = createNewTask(taskText);
     document.getElementById("activeTasks").appendChild(newTask);
     taskInput.value = "";
     saveData();
   } else {
-    window.alert("Please enter valid task.");
+    window.alert("Please enter a task, that is not empty. ");
   }
 }
 
 // CREATING NEW TASK ITEMS:
-
 function createNewTask(taskText) {
   var newTask = document.createElement("li");
   newTask.draggable = "true";
@@ -43,7 +42,7 @@ function createNewTask(taskText) {
   var checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.addEventListener("change", function () {
-    moveTask(newTask, checkbox.checked);
+    completingTasks(newTask, checkbox.checked);
     saveData();
   });
 
@@ -60,8 +59,7 @@ function createNewTask(taskText) {
 }
 
 // MOVING TASKS:
-
-function moveTask(newTask, completed) {
+function completingTasks(newTask, completed) {
   var activeTasksList = document.getElementById("activeTasks");
   var completedTasksList = document.getElementById("completedTasks");
 
@@ -73,27 +71,24 @@ function moveTask(newTask, completed) {
 }
 
 // EDITING TASKS:
-
 function editTask(newTask) {
   var newText = prompt("Edit task:", newTask.querySelector("span").textContent);
-  if (newText !== null && newText.trim() !== "") {
+  if (newText != null && newText.trim() != "") {
     newTask.querySelector("span").textContent = newText.trim();
     saveData();
-  } else if (newText === "") {
+  } else if (newText == "") {
     newTask.remove();
     saveData();
   }
 }
 
 // DELETING TASKS:
-
 function deleteTask(newTask) {
   newTask.remove();
   saveData();
 }
 
 // SAVING TASKS:
-
 function saveData() {
   var tasks = [];
   var taskElements = document.querySelectorAll(
@@ -103,7 +98,7 @@ function saveData() {
   taskElements.forEach(function (task) {
     tasks.push({
       text: task.querySelector("span").textContent,
-      completed: task.parentElement.id === "completedTasks",
+      completed: task.parentElement.id == "completedTasks",
     });
   });
 
@@ -111,7 +106,6 @@ function saveData() {
 }
 
 // LOADING TASKS:
-
 function loadData() {
   var tasks = JSON.parse(localStorage.getItem("tasks"));
 
@@ -131,22 +125,21 @@ function loadData() {
 window.addEventListener("load", loadData);
 
 // REORDERING TASKS:
+function sortingTasks() {
+  var activeTasksList = document.getElementById("activeTasks");
+  var completedTasksList = document.getElementById("completedTasks");
 
-function initializeSortableLists() {
-  const activeTasksList = document.getElementById("activeTasks");
-  const completedTasksList = document.getElementById("completedTasks");
-
-  const activeTasksSortable = new Sortable(activeTasksList, {
+  var activeTasksSortable = new Sortable(activeTasksList, {
     group: "tasks",
-    animation: 150,
+    animation: 300,
     onUpdate: function (event) {
       saveReorderedTasks(event.from.id, event.to.id);
     },
   });
 
-  const completedTasksSortable = new Sortable(completedTasksList, {
+  var completedTasksSortable = new Sortable(completedTasksList, {
     group: "tasks",
-    animation: 150,
+    animation: 300,
     onUpdate: function (event) {
       saveReorderedTasks(event.from.id, event.to.id);
     },
@@ -157,4 +150,4 @@ function initializeSortableLists() {
   }
 }
 
-initializeSortableLists();
+sortingTasks();
